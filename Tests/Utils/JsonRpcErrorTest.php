c5fb8a213316148cc4757dac96eed776f0dbcd35
Evgeniy <evgeniy@neofusion.ru>
2016-11-24 17:21:20 +0300

    Add Utils tests

---
+++ Tests/Utils/JsonRpcErrorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace NeoFusion\JsonRpcBundle\Tests\Utils;
+
+use NeoFusion\JsonRpcBundle\Utils\JsonRpcError;
+
+class JsonRpcErrorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCode()
+    {
+        $jsonRpcError = new JsonRpcError(JsonRpcError::CODE_SERVER_ERROR);
+        $this->assertEquals(array(
+            'code'    => -32000,
+            'message' => 'Server error'
+        ), $jsonRpcError->toArray());
+    }
+
+    public function testMessage()
+    {
+        $jsonRpcError = new JsonRpcError(JsonRpcError::CODE_SERVER_ERROR, 'Custom message');
+        $this->assertEquals(array(
+            'code'    => -32000,
+            'message' => 'Custom message'
+        ), $jsonRpcError->toArray());
+    }
+
+    public function testData()
+    {
+        $jsonRpcError = new JsonRpcError(JsonRpcError::CODE_SERVER_ERROR, null, 'Custom data');
+        $this->assertEquals(array(
+            'code'    => -32000,
+            'message' => 'Server error',
+            'data'    => 'Custom data'
+        ), $jsonRpcError->toArray());
+    }
+}
---
+++ Tests/Utils/JsonRpcSingleRequestTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NeoFusion\JsonRpcBundle\Tests\Utils;
+
+use NeoFusion\JsonRpcBundle\Utils\JsonRpcSingleRequest;
+
+class JsonRpcSingleRequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValidRequest()
+    {
+        $jsonRpcSingleRequest = new JsonRpcSingleRequest('2.0', 'create', array(42), 1);
+
+        $this->assertEquals('2.0', $jsonRpcSingleRequest->getJsonrpc());
+        $this->assertEquals('create', $jsonRpcSingleRequest->getMethod());
+        $this->assertEquals(array(42), $jsonRpcSingleRequest->getParams());
+        $this->assertEquals(1, $jsonRpcSingleRequest->getId());
+
+        $this->assertTrue($jsonRpcSingleRequest->isValid());
+        $this->assertFalse($jsonRpcSingleRequest->isNotification());
+    }
+
+    public function testNotification()
+    {
+        $jsonRpcSingleRequest = new JsonRpcSingleRequest('2.0', 'create', array(42), null);
+        $this->assertTrue($jsonRpcSingleRequest->isValid());
+        $this->assertTrue($jsonRpcSingleRequest->isNotification());
+    }
+
+    public function testInvalidParams()
+    {
+        $jsonRpcSingleRequest = new JsonRpcSingleRequest('2.0', 'create', 42, 1);
+        $this->assertFalse($jsonRpcSingleRequest->isValid());
+    }
+
+    public function testInvalidId()
+    {
+        $jsonRpcSingleRequest = new JsonRpcSingleRequest('2.0', 'create', array(42), array(1));
+        $this->assertFalse($jsonRpcSingleRequest->isValid());
+    }
+}
